64cf67ac8f85fbea79f0d761b50d6a4ee179d132
Casey Cabrales <caseycabrales@gmail.com>
2017-06-01 20:38:35 -0700

    build: Add main server, basic client setup, build settings

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const debug = require('debug')('app:api server');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  debug('Server is being run in production, meaning it will only serve static assets');
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  debug(`Server is now running on port ${port}`);
+});
