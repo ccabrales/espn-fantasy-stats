3fc553e6bbd4c71f49aad875781c3fda9cb0e5ed
Casey Cabrales <caseycabrales@gmail.com>
2017-06-01 23:01:07 -0700

    build: Add index serve back to server

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   debug(`Server is now running on port ${port}`);
