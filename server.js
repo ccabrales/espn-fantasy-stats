050733fc6d8670bf3b8c0d8e84744fa5d1885848
Casey Cabrales <caseycabrales@gmail.com>
2017-06-01 23:41:05 -0700

    ci: Test fix for heroku

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+app.get('/api/test', (req, res) => {
+  res.json({ hello: 'there' });
 });
 
 const port = process.env.PORT || 3001;
